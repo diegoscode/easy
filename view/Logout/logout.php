983cdc07b61c8226585404ad3ac5d18c68e51091
diegoscode <diegobravo2030@gmail.com>
2023-02-04 01:41:26 -0400

    se agrega pagina de inicio

--- view/Logout/logout.php
+++ view/Logout/logout.php
@@ -1,6 +1,6 @@
 <?php
     require_once("../../config/conexion.php");
     session_destroy();
-    header("Location:".Conectar::ruta()."index.php");
+    header("Location:".Conectar::ruta()."login.php");
     exit();
 ?>
